9282238c82291253540cb08f094078ae5fce3ba8
Derek Mosher <derekmosher21@gmail.com>
2020-07-21 17:19:24 -0400

    completed test for threeEqualSidesIsAnEquilateralTriangle

--- src/test/java/triangles/TrianglesTest.java
+++ src/test/java/triangles/TrianglesTest.java
@@ -6,10 +6,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class TrianglesTest {
+
+
     @Test
     public void threeEqualSidesIsAnEquilateralTriangle() {
         TriangleSorter underTest = new TriangleSorter();
         String triangleType = underTest.analyze(3, 3, 3);
         assertThat(triangleType).isEqualTo("Equilateral");
     }
+
+
 }
