8b265d33b59ab8ef10b2f7337818f6e7920d65a7
Kendyl Hull <hullkendyl@gmail.com>
2020-07-23 09:32:55 -0400

    completed tests for oneHourPreBedTimeShould Pay 12 & twoHoursPreBedTimeShouldPay24

--- src/main/java/babysitter/WageCalculator.java
+++ src/main/java/babysitter/WageCalculator.java
@@ -1,19 +1,42 @@
 package babysitter;
 
 public class WageCalculator {
-    int wage;
 
-    public int calculateShiftWage(int startingHour, int endingHour, int bedtime) {
+    int preBedTimeWage = 12;
+    int preBedTimeEarnings;
+//    int BedTimeWage;
+//    int MidnightWage;
+
+    public int calculatePreBedTimeEarnings(int startingHour, int endingHour, int bedtime) {
         if (startingHour < bedtime && endingHour < bedtime) {
-            return wage = 12;
-        } else if (endingHour > bedtime && endingHour <= 24) {
-            return wage = 8;
-        } else if (endingHour > 24 ) {
-            return wage = 16;
-        }
-
-        else {
-            return 0;
+            return preBedTimeEarnings = preBedTimeWage * (endingHour - startingHour);
+        } else {
+            return preBedTimeEarnings = 0;
         }
     }
 }
+//    public int calculateBedTimeWage(int startingHour, int endingHour, int bedtime) {
+//
+//    }
+//
+//    public int MidnightWage(int startingHour, int endingHour, int bedtime) {
+//
+//    }
+
+//    public int calculateShiftWage(int startingHour, int endingHour, int bedtime) {
+//        if (startingHour < bedtime && endingHour < bedtime) {
+//            return wage = 12;
+//        } else if (endingHour > bedtime && endingHour <= 24) {
+//            return wage = 8;
+//        } else if (endingHour > 24 ) {
+//            return wage = 16;
+//        }
+//
+//        else {
+//            return 0;
+//        }
+//    }
+//
+
+//    (wages1*shiftonehours1)+(w2*s2)+{w3*s3)= totalPay}
+
--- src/test/java/babysitter/BabySitterTest.java
+++ src/test/java/babysitter/BabySitterTest.java
@@ -6,25 +6,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class BabySitterTest {
+
     @Test
     public void oneHourPreBedTimeShouldPay12() {
         WageCalculator underTest = new WageCalculator();
-        int wage = underTest.calculateShiftWage(17, 18, 20);
-        assertThat(wage).isEqualTo(12);
+        int preBedTimeWage = underTest.calculatePreBedTimeEarnings(17, 18, 20);
+        assertThat(preBedTimeWage).isEqualTo(12);
     }
 
     @Test
-    public void oneHourBetweenBedTimeAndMidnightShouldPay8() {
+    public void twoHoursPreBedTimeShouldPay24() {
         WageCalculator underTest = new WageCalculator();
-        int wage = underTest.calculateShiftWage(18, 24, 23);
-        assertThat(wage).isEqualTo(8);
+        int preBedTimeEarnings = underTest.calculatePreBedTimeEarnings(17, 19, 20);
+        assertThat(preBedTimeEarnings).isEqualTo(24);
     }
 
-    @Test
-    public void oneHourPostMidnightShouldPay16() {
-        WageCalculator underTest = new WageCalculator();
-        int wage = underTest.calculateShiftWage(18, 27, 23);
-        assertThat(wage).isEqualTo(16);
-    }
-
+//    @Test
+//    public void oneHourBetweenBedTimeAndMidnightShouldPay8() {
+//        WageCalculator underTest = new WageCalculator();
+//        int wage = underTest.calculateShiftWage(18, 24, 23);
+//        assertThat(wage).isEqualTo(8);
+//    }
+//
+//    @Test
+//    public void oneHourPostMidnightShouldPay16() {
+//        WageCalculator underTest = new WageCalculator();
+//        int wage = underTest.calculateShiftWage(18, 27, 23);
+//        assertThat(wage).isEqualTo(16);
+//    }
+//
+//    @Test
+//    public void calculateShiftWages() {
+//        WageCalculator underTest = new WageCalculator();
+//        int totalPay = underTest.calculateNightlyEarnings()
+//    }
 }
